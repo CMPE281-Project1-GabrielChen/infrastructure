fa843c04c870aac457256fbdbc23bdb7a0b1e825
Gabriel Chen <gabriel.luke.chen@gmail.com>
2021-10-18 12:07:34 -0700

    WIP: stuff

--- main.tf
+++ main.tf
@@ -68,6 +68,8 @@ resource "aws_cloudfront_distribution" "cf_user_file_store_cache" {
 	comment = "CF Distro for User File Storage Cache"
 
 	default_cache_behavior {
+		trusted_key_groups = [aws_cloudfront_key_group.file_store_keygroup.id]
+
 		allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
 		cached_methods = ["GET", "HEAD"]
 		target_origin_id = aws_s3_bucket.s3_user_file_store.id
@@ -106,14 +108,21 @@ resource "aws_cloudfront_origin_access_identity" "cf_user_file_store_oai" {
 	comment = "origin access identity for s3_user_file_store bucket"
 }
 
+data "aws_iam_role" "file_management_lambdas_role" {
+	name = "file-management-api-dev-us-west-2-lambdaRole"
+}
+
 data "aws_iam_policy_document" "user_file_store_s3_policy" {
 	statement {
 		actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
-		resources = ["${aws_s3_bucket.s3_user_file_store.arn}/*"]
+		resources = ["${aws_s3_bucket.s3_user_file_store.arn}/*"] 
 
 		principals {
 			type = "AWS"
-			identifiers = [aws_cloudfront_origin_access_identity.cf_user_file_store_oai.iam_arn]
+			identifiers = [
+				aws_cloudfront_origin_access_identity.cf_user_file_store_oai.iam_arn,
+				data.aws_iam_role.file_management_lambdas_role.arn
+			]
 		}
 	}
 }
@@ -135,20 +144,142 @@ resource "aws_cloudfront_key_group" "file_store_keygroup" {
 	name = "${terraform.workspace}-file-store-keygroup"
 }
 
-# resource "aws_dynamodb_table" "files_table" {
-# 	name = "${terraform.workspace}-files"
-# 	billing_mode = "PROVISIONED"
-# 	read_capacity = 1
-# 	write_capacity = 1
+resource "aws_secretsmanager_secret" "file_management_private_key" {
+	name = "${terraform.workspace}-file-management-private-key"
+}
 
-# 	hash_key = "FileID"
+resource "aws_secretsmanager_secret" "file_management_public_id" {
+	name = "${terraform.workspace}-file-management-public-id"
+}
 
-# 	attribute {
-# 		name = "FileID"
-# 		type = "S"
-# 	}
+resource "aws_dynamodb_table" "files_table" {
+	name = "${terraform.workspace}-files"
+	billing_mode = "PROVISIONED"
+	read_capacity = 1
+	write_capacity = 1
+	hash_key = "FileID"
+
+	attribute {
+		name = "FileID"
+		type = "S"
+	}
 	
-# 	attribute {
-# 		name = "UserID"
-# 	}
-# }
+	attribute {
+		name = "UserID"
+		type = "S"
+	}
+
+	global_secondary_index {
+		name = "FileIDIndex"
+		hash_key = "FileID"
+		write_capacity = 1
+		read_capacity = 1
+		projection_type = "KEYS_ONLY"
+	}
+
+	global_secondary_index {
+	  name = "UserIDIndex"
+	  hash_key = "UserID"
+	  write_capacity = 1
+	  read_capacity = 1
+	  projection_type = "KEYS_ONLY"
+	}
+
+	tags = {
+		Name = "${terraform.workspace}-files-dynamodb-table"
+		Environment = "${terraform.workspace}"
+	}
+}
+
+#################### Front End Resources #########################3
+resource "aws_s3_bucket" "s3_cloud_drive_front_end" {
+	bucket = "${terraform.workspace}-cloud-drive-app"
+	acl = "private"
+
+	versioning {
+		enabled = true
+	}
+}
+
+resource "aws_s3_bucket_public_access_block" "block_all_public_access_cloud_drive_front_end" {
+  bucket = aws_s3_bucket.s3_cloud_drive_front_end.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_cloudfront_distribution" "cf_cloud_drive_front_end" {
+	origin {
+		domain_name = aws_s3_bucket.s3_cloud_drive_front_end.bucket_regional_domain_name
+		origin_id = aws_s3_bucket.s3_cloud_drive_front_end.id
+
+		s3_origin_config {
+		  origin_access_identity = aws_cloudfront_origin_access_identity.cf_cloud_drive_front_end_oai.cloudfront_access_identity_path
+		}
+	}
+
+	default_root_object = "index.html"
+
+	enabled = true
+	is_ipv6_enabled = true
+	comment = "CF Distro for User File Storage Cache"
+
+	default_cache_behavior {
+		allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+		cached_methods = ["GET", "HEAD"]
+		target_origin_id = aws_s3_bucket.s3_cloud_drive_front_end.id
+
+		viewer_protocol_policy = "allow-all"
+		min_ttl                = 0
+    	default_ttl            = 0 
+    	max_ttl                = 0 
+
+		forwarded_values {
+		  query_string = false
+
+		  cookies {
+			  forward = "none"
+		  }
+		}
+	}
+
+	tags = {
+		Environment = "${terraform.workspace}"
+	}
+
+	restrictions {
+	  geo_restriction {
+		  restriction_type = "whitelist"
+		  locations = ["US"]
+	  }
+	}
+
+	viewer_certificate {
+	  cloudfront_default_certificate = true
+	}
+}
+
+resource "aws_cloudfront_origin_access_identity" "cf_cloud_drive_front_end_oai" {
+	comment = "origin access identity for cloud_drive_front_end bucket"
+}
+
+data "aws_iam_policy_document" "cloud_drive_front_end_s3_policy" {
+	statement {
+		actions = ["s3:GetObject"]
+		resources = ["${aws_s3_bucket.s3_cloud_drive_front_end.arn}/*"] 
+
+		principals {
+			type = "AWS"
+			identifiers = [
+				aws_cloudfront_origin_access_identity.cf_cloud_drive_front_end_oai.iam_arn
+			]
+		}
+	}
+}
+
+resource "aws_s3_bucket_policy" "s3_cloud_drive_front_end_policy_attach" {
+	bucket = aws_s3_bucket.s3_cloud_drive_front_end.id
+	policy = data.aws_iam_policy_document.cloud_drive_front_end_s3_policy.json
+}
